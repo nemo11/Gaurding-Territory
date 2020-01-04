2ab390ce662185ad1701ae3771331d5cf0faeaf2
nemo11 <nishantmohanty18@gmail.com>
2020-01-04 11:33:01 -0800

    first commit

---
+++ environment.py
@@ -0,0 +1,101 @@
+import numpy as np
+from math import sin,atan2,cos
+import random
+import matplotlib.pyplot as plt 
+
+class Env():
+    def __init__(self):
+        self.arena_size = [100,100]             # [length,breadth] magnitude
+        self.arena_diagonal = [[-50,50],[50,-50]]
+        self.gravity_point = [0,0]
+        # attack or defense
+        self.force_direction = +1 
+        # regulator is to control the offense and defense
+        # relative distance is the distance of the vehicle from the gravity point
+        self.regulator = 1
+        self.K = 1               # gravitational constant
+        self.relative_distance = 1
+        self.force_magnitude = self.K*(self.regulator)/(self.relative_distance)                     # the force magnitude to regulate the path
+        # opponents force
+        self.opponent_pos = []
+        self.number_of_opponents = 10
+        self.opponent_force = 0
+        self.opponent_radial_velocity = 5
+        self.dt = 0.01          ##discreet time
+
+    def radial_force(self):
+        force_radial = self.force_magnitude - self.opponent_force
+        return force_radial
+
+
+
+#################################################
+    ###Opponents section of the code
+
+    def opponent_model(self):
+
+        if len(self.opponent_pos) != self.number_of_opponents:         
+            for i in range (self.number_of_opponents):
+                initial_pos = self.opponent_restart()
+                self.opponent_pos.append(initial_pos)
+        else:
+            for i in range (self.number_of_opponents):
+                (v_x,v_y) = self.opponent_control(self.opponent_pos[i])
+
+                #dynamics of the opponent
+                self.opponent_pos[i][0] = self.opponent_pos[i][0] + v_x*self.dt 
+                self.opponent_pos[i][1] = self.opponent_pos[i][1] + v_y*self.dt 
+
+                #check if the target is nullified
+                nullify_status = self.opponent_check_if_nullified(self.opponent_pos[i])
+                if nullify_status == 1:
+                    del self.opponent_pos[i]
+
+        return self.opponent_pos        
+
+    def opponent_check_if_nullified(self,pos):                 ##to check if nullified or not
+
+        nullify_status = 0
+
+        # close to the point of gravity
+        if (pos[0] - self.gravity_point[0])**2 + (pos[1] - self.gravity_point[1])**2 <= 2:
+            nullify_status = 1
+  
+        # close to gaurd team
+
+          ###TO BE WRITTEN
+
+        return nullify_status
+
+
+    def opponent_restart(self):
+
+        random_corner = random.randrange(0,2)
+        random_side = random.randrange(0,2)
+        if random_side == 0:                
+            initial_pos = [self.arena_diagonal[random_corner][0] , random.randrange(-50,50)]            ##Need to change this to more general
+        if random_side == 1:                
+            initial_pos = [random.randrange(-50,50) , self.arena_diagonal[random_corner][1]] 
+
+        return initial_pos    
+
+
+    def opponent_control(self,pos):
+
+        theta = atan2((pos[1] - self.gravity_point[1]),(pos[0] - self.gravity_point[0]))        #angle of the opponent with the gravity point
+        velocity_x = -(self.force_direction)*self.opponent_radial_velocity*cos(theta)
+        velocity_y = -(self.force_direction)*self.opponent_radial_velocity*sin(theta)
+
+        return velocity_x,velocity_y
+
+###########################*********************** Above part is the Opponent Section of the code ***************###############################
+			
+if __name__ == '__main__':
+    env = Env()
+    while True:
+        env.opponent_model()
+        for i in range (env.number_of_opponents):
+            print("hello")
+            plt.plot(env.opponent_pos[i][0],env.opponent_pos[i][1],'r.')
+
+
